90661ceae181b77378d2f81bfce265f4de5e8bc1
Lana <115147586+ricorett@users.noreply.github.com>
2024-01-27 22:16:06 +0300

    try to fix create matrix

--- src/funcs/s21_create_matrix.c
+++ src/funcs/s21_create_matrix.c
@@ -20,7 +20,7 @@ int s21_create_matrix(int rows, int columns, matrix_t *result) {
   }
 
   for (int i = 0; i < rows; i++) {
-    result->matrix[i] = calloc(columns, sizeof(double));
+    result->matrix[i] = (double*)calloc(columns, sizeof(double));
     if (result->matrix[i] == NULL) {
       for (int j = 0; j < columns; j++) {
         free(result->matrix[j]);
